8a6c0fee13b0c3e1df3d2f002a4a380f8c02a7b7
Tashby <tashby2714@gmail.com>
2016-07-20 23:30:21 -0400

    javascript react

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-document.write(require("./content.js"));
+import React from 'react';
+
+const todos = [
+{
+	task: 'make React tutorial';
+	isCompleted: false
+}
+];
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1> React To Do App</h1>
+			</div>
+			);
+	}
+}
