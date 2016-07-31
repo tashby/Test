d566739faa313dd42abf3d35c93bb5c80d157cb9
Tashby <tashby2714@gmail.com>
2016-07-30 23:20:51 -0400

    changed component to contacts

---
+++ components/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ContactsList extends React.Component {
+    render() {
+        return ( 
+            <ul>
+                 <li>{this.props.contacts.name} {this.props.contacts.phone}</li>
+            </ul>
+            )
+    }
+}
+
+export default ContactsList;
--- index.jsx
+++ index.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Person from './components/Person';
+import ContactsList from './ContactsList';
 
-render(<Person />, document.getElementById('app'));
+let contacts = {
+	name:'Scott',
+	phone:'555 555 5555'
+}
+
+class App extends React.Component {
+	render() {
+			consolelog(this.props.contacts)
+			return (
+<div>
+	<h1>ContactsList</h1>
+	<ContactsList contacts= {this.props.contacts} />
+
+</div>
+				)
+	}	
+}
+
+React.render(<ContactsList />, document.getElementById('app'));
