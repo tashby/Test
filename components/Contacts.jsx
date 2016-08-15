be31272187ad2f38297e468ef980448fdf01729b
Tashby <tashby2714@gmail.com>
2016-08-14 23:23:50 -0400

    user widgets

--- components/Contacts.jsx
+++ components/Contacts.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import Avatar from 'material-ui/Avatar';
+import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
 
-class ContactsList extends React.Component {
-    render() {
-        return ( 
-            <ul>
-                 <li>{this.props.contacts.name} {this.props.contacts.phone}</li>
-            </ul>
+import FontIcon from 'material-ui/FontIcon';
+import {red500, yellow500, blue500} from 'material-ui/styles/colors';
 
-            <div></div>
-            <h1>
-            <h2> [this.props.contacts.name]
-            )
-    }
+const iconStyles = {
+  marginRight: 24,
+};
+
+const UserWidget = (props) => (
+  <div>
+      <List>
+        <ListItem
+          leftAvatar={<Avatar src={props.user.picture.thumbnail} />}
+          primaryText={props.user.cell}
+          secondaryText={
+            <p>
+              <span style={{color: darkBlack}}>{props.user.name.first} {props.user.name.last}</span> --
+              {props.user.location.city}
+              <FontIcon onClick={props.deleteUser} className="material-icons" style={iconStyles} color={red500}>delete_forever</FontIcon>
+            </p>
+          }
+          secondaryTextLines={2}
+        />
+        <Divider inset={true} />
+      </List>
+  </div>
+);
+
+UserWidget.PropTypes = {
+  user: React.PropTypes.object,
+  deleteUser: React.PropTypes.func
 }
 
-export default ContactsList;
+export default UserWidget;
