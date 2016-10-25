9560a89e017c203904c1e96f72d246904e8d856d
Tashby <tashby2714@gmail.com>
2016-10-24 23:49:21 -0400

    oct24

--- index.jsx
+++ index.jsx
@@ -102,7 +102,10 @@ div className="App-header"></div>
         {this.state.users.map((user) => <UserWidget key={user.cell} user={user} /> )}
 
 div classdget key={user.cell} user={user} /> )}
+div className="App-header"></div>
+        {this.state.users.map((user) => <UserWidget key={user.cell} user={user} /> )}
 
+div classdget key={user.cell} user={user} /> )}
 div className="App-header"></div>
         {this.state.users.map((user) => <UserWidget key={user.cell} user={user} /> )}
           
