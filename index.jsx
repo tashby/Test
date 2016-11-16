143009b5a214e0333b9721b12912febe688be35a
Tashby <tashby2714@gmail.com>
2016-11-15 23:58:33 -0500

    nov 15

--- index.jsx
+++ index.jsx
@@ -80,15 +80,6 @@ A.
       </div>
     return (
       <div className="App">
-        <div className="App-header"></div>
-        {this.state.users.map((user) => <UserWidget key={user.cell} user={user} /> )}
-      </div>
-    return (
-      <div className="App">
-        <div className="App-header"></div>
-        {this.state.users.map((user) => <UserWidget key={user.cell} user={user} /> )}
-      </div><div className="App">
-        <div className="App-header"></div>
 
 
 <div className="App-header"></div>
