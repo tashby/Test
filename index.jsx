491e598c8fb1cfbf26efcbb06bec9daa7895dfdc
Tashby <tashby2714@gmail.com>
2016-11-20 23:53:12 -0500

    new user nov20

--- index.jsx
+++ index.jsx
@@ -129,8 +129,7 @@ div className="App-header"></div>
    
 div classdget key={user.cell} user={user} /> )}
 div className="App-header"></div>
-        {this.state.users.map((user) => <UserWidget key={user.cell} user={user} /> )}
-user.cell} user={user} /> )}
+
           
 div classdget key={user.cell} user={user} /> )}
 div className="App-header"></div>
