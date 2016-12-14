205a82a4e170ce115397d236561a6cc5462aa9e7
Tashby <tashby2714@gmail.com>
2016-12-13 22:08:00 -0500

    dec 13

--- index.jsx
+++ index.jsx
@@ -131,5 +131,6 @@ div className="App-header"></div>
         {this.state.users.map((user) => <Udiv className="App-header"></div>
         {this.state.users.map((user) => <U      div classdget key={user.cell} user={user} /> )}
 div className="App-header"></div>
+        {this.state.users.map((user) => <Udiv className="App-header"></div>
         {this.state.users.map((user) => <U
 export default App;
