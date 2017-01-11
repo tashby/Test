b01deef2cbd792d5bc1b506f87f495c91bc2c7aa
Tashby <tashby2714@gmail.com>
2017-01-10 22:26:26 -0500

    jan11

--- index.jsx
+++ index.jsx
@@ -111,6 +111,17 @@ div className="App-header"></div>
    div classdget key={user.cell} user={user} /> )}
 div className="App-header"></div>
    
+ className="App-header"></div>
+        {this.state.users.map((user) => <UserWidget key={user.cell} user={user} /> )}
+      iv classdget key={user.cell} user={user} /> )}
+div classdget key={user.cell} user={user} /> )}
+
+div classdget key={user.cell} user={user} /> )}
+div className="App-header"></div>
+   div classdget key={user.cell} user={user} /> )}
+div className="App-header"></div>
+   
+
 
 export default App;
 
