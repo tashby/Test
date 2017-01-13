f31146d285725dc208d77435642e742a6e56d046
Tashby <tashby2714@gmail.com>
2017-01-13 02:16:16 -0500

    jan13

--- index.jsx
+++ index.jsx
@@ -132,7 +132,30 @@ div classdget key={user.cell} user={user} /> )}
 div className="App-header"></div>
    div classdget key={user.cell} user={user} /> )}
 div className="App-header"></div>
+    className="App-header"></div>
+        {this.state.users.map((user) => <UserWidget key={user.cell} user={user} /> )}
+      iv classdget key={user.cell} user={user} /> )}
+div classdget key={user.cell} user={user} /> )}
+
+div classdget key={user.cell} user={user} /> )}
+div className="App-header"></div>
+   div classdget key={user.cell} user={user} /> )}
+div className="App-header"></div>
+   div classdget key={user.cell} user={user} /> )}
+div className="App-header"></div>
+   div classdget key={user.cell} user={user} /> )}
+div className="App-header"></div>
+ className="App-header"></div>
+        {this.state.users.map((user) => <UserWidget key={user.cell} user={user} /> )}
+      iv classdget key={user.cell} user={user} /> )}
+div classdget key={user.cell} user={user} /> )}
+
+div classdget key={user.cell} user={user} /> )}
+div className="App-header"></div>
+   div classdget key={user.cell} user={user} /> )}
+div className="App-header"></div>
    
+
 
 
 export default App;
